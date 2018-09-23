2bff55f0fc439df201018084aa9b8962a107ab9a
Luciano Longo <luciano.longo@gmail.com>
2018-09-23 18:52:48 -0300

    Handle Reach Router redirections

--- server/renderApp.js
+++ server/renderApp.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
-import { ServerLocation } from '@reach/router'
+import { isRedirect, ServerLocation } from '@reach/router'
 import serialize from 'serialize-javascript'
 import Document from '../src/Document'
 import App from '../src/App'
 
 export default function renderApp (req, res) {
   return initialProps => {
-    const html = ReactDOMServer.renderToString(
-      <ServerLocation url={req.url}>
-        <Document>
-          <App {...initialProps} />
-        </Document>
-      </ServerLocation>
-    )
-    res.send(html.replace('@@INITIAL_PROPS@@', serialize(initialProps)))
+    try {
+      const html = ReactDOMServer.renderToString(
+        <ServerLocation url={req.url}>
+          <Document>
+            <App {...initialProps} />
+          </Document>
+        </ServerLocation>
+      )
+      res.send(html.replace('@@INITIAL_PROPS@@', serialize(initialProps)))
+    } catch (error) {
+      if (isRedirect(error)) {
+        return res.redirect(error.uri)
+      }
+      throw error
+    }
   }
 }
