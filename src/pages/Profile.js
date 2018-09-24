0571f56d50dca96a18a3d8daa7e03db155f020a4
Luciano Longo <luciano.longo@valtech.com>
2018-09-24 16:03:40 -0300

    Add demo of context parameters passed to getInitialProps

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,17 +1,23 @@
 import React from 'react'
 
 export default class Profile extends React.PureComponent {
-  static async getInitialProps ({ params }) {
+  static async getInitialProps ({ path, params, uri }) {
     return {
-      profile: params.profileSlug
+      profile: params.profileSlug,
+      context: { path, params, uri }
     }
   }
 
   render () {
-    const { profile } = this.props
+    const { profile, context } = this.props
     return (
       <div>
         <h1>Profile {profile}!</h1>
+
+        <h2>This is the <code>context</code> object that is passed to <code>getInitialProps</code>:</h2>
+        <pre>{
+          context && JSON.stringify(context, null, '  ')
+        }</pre>
       </div>
     )
   }
